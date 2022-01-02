a36056839afecff285a2eea1640c1c196a0d66f9
Leonardo Jose Nunes <leonardo.ferax@gmail.com>
2022-01-02 15:14:35 -0300

    Routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const fs = require("fs")
 const http = require("http")
+const url = require("url")
 
 /////////////////////////////////
 // FILES
@@ -40,7 +41,22 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {
   // console.log(req)
-  res.end("Hello from the server!")
+  console.log(req.url)
+  // res.setHeader("Content-Type", "text/html", "utf-8")
+  const pathName = req.url
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW")
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT")
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world"
+    })
+
+    res.end(`<html><body><h1>PAGE NOT FOUND</h1></body></html>`)
+  }
 })
 
 server.listen(8000, "127.0.0.1", () => {
